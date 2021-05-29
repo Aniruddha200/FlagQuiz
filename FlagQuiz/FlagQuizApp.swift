d2eaf046dfa2c9835bd2ad2d32fd8ab4b1a1cdfa
aniruddha <aniruddha200baidya@protonmail.com>
2021-05-29 11:40:59 +0100

    Initial Commit

---
+++ FlagQuiz/FlagQuizApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlagQuizApp.swift
+//  FlagQuiz
+//
+//  Created by administrator on 29/05/2021.
+//
+
+import SwiftUI
+
+@main
+struct FlagQuizApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
